dab33cc1466060ef54e586542ed52eb535d2d0da
foenix <foenickz@gmail.com>
2010-04-20 10:52:36 -0700

    initial commit

---
+++ mouth.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+The mouth of ibnalnais is a file of all such things which may have once been
+considered a "view" or "controller".
+I like mouths, though. Mouths are truly where most of our cogntion finds it's
+end-point (and if your mind is in the gutter, that's alright, most of the stuff
+pushed out of the mind is crap anyways).
+
+If you haven't guessed it yet, ibnalnafis is a direct referece to:
+Ala al-Din Abu al-Hassan Ali ibn Abi-Hazm al-Qurashi al-Dimashqi
+علاء الدين أبو الحسن عليّ بن أبي حزم القرشي الدمشقي 
+
+Or, as his mates prefered to call him: ابن النفيس
+(IPA: ĭbn ul-næfis)
+Ibn al-Nafis: http://en.wikipedia.org/wiki/Ibn_al-Nafis
+
+This fine gentleman studied the vascular system of the human anatomy. He
+brought together many advanced ideas in his day to do so; and it showed in his
+demeanor. He studed many things: linguistics, futurology,
+primordial-psychology, logic, literature, sociology, geology, medicine — why
+not do the same in his honour with a well-wrought CRM. Something from the
+ground-up and able to be applied to a number of things.
+
+Anyways, this app is basically a document broadcaster which implements rss,
+ranking, and relational magix to relay the words of whomever around. 
+
+The primary goal of this project is to bring together current state-of-the-art
+web-programming. A secondary priority is mega-scalability. This site is
+currently designed with a fair amount of use in mind (and not just http).
+"""
+__author__='foenix'
+__version__='0.0.0'
+__license='MIT' # sure, why not.
+
+# Todo Everything
