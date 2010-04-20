473de31057502d3b22bd5829a6951494626ae20f
foenix <foenickz@gmail.com>
2010-04-20 14:20:14 -0700

    Let's do this...

---
+++ glial.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+"""
+Glial cells provide structural support and metabolic support to cells within
+the central nervous system. They also are important in the arrangement of
+neurons throughout development.
+
+For this app, glial.py is the primary means by which we route urls
+"""
---
+++ hebbian.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+"""
+Hebbian potentiation refers to the means by which a neuron will have an
+increased activation when it is used. It is the primary means by which the
+brain stores protocols, and provides evidence for an emergent connectionist
+network in cognitive function as opposed to a symbolic mental lexicon.
+
+In this case, this file is used to set up the application's "models". A loose
+term, when one considers we are storing to an object database, as opposed to a
+relational one.
+
+The application's objects and relations are defined in this file.
+"""
+
